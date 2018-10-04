013ec9ff16669fdaf8b63944018a69d97da744d4
Claudio Lepin <azratul@gmail.com>
2018-10-04 08:55:40 -0300

    Improvements

--- battleship.py
+++ battleship.py
@@ -3,45 +3,30 @@
 import os
 import getpass
 
-PLAYER1 = 'PRINCIPIANTES'
-PLAYER2 = 'DA IGUAL'
-BOARD   = [26,26] # rows(infinito),cols(máx. 26)
-HIT     = '\033[1;32m★\033[0;37m'
-MISS    = '\033[1;31m✕\033[0;37m'
-EMPTY   = '- '
+PLAYERS = ['PRINCIPIANTES', 'DA IGUAL']
+BOARD   = [40,26] # rows(máx. 99),cols(máx. 26)
 
-team         = [[],[]]
-team_repeat  = [[],[]]
-cols         = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-board_team1  = [[EMPTY for i in range(BOARD[0])] for j in range(BOARD[1])]
-board_team2  = [[EMPTY for i in range(BOARD[0])] for j in range(BOARD[1])]
+team        = [[],[]]
+team_repeat = [[],[]]
+cols        = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+board_team  = [[['⃞ ' for i in range(BOARD[0])] for j in range(BOARD[1])],[['⃞ ' for i in range(BOARD[0])] for j in range(BOARD[1])]]
 
 def fill_board(shoot, turn):
 	os.system('clear')
 	for x in xrange(BOARD[0]):
 		for y in range(0, BOARD[1]):
 			if shoot == cols[y] + str(x + 1):
-				if turn == PLAYER1:
-					if shoot in team[1]:
-						team[1].remove(shoot)
-						board_team2[y][x] = HIT
-					else:
-						board_team2[y][x] = MISS
-					team_repeat[0].append(shoot)
-
-				if turn == PLAYER2:
-					if shoot in team[0]:
-						team[0].remove(shoot)
-						board_team1[y][x] = HIT
-					else:
-						board_team1[y][x] = MISS
-					team_repeat[1].append(shoot)
-
+				if shoot in team[1-turn]:
+					team[1-turn].remove(shoot)
+					board_team[1-turn][y][x] = '\033[1;32m★\033[0;37m'
+				else:
+					board_team[1-turn][y][x] = '\033[1;31m✕\033[0;37m'
+				team_repeat[turn].append(shoot)
 	draw_board()
 
 def draw_board():
-	title1 = (' ' * (BOARD[1] - (len(PLAYER1) / 2))) + '\033[1;37m' + PLAYER1 + '\033[0;37m' + (' ' * (BOARD[1] - (len(PLAYER1) / 2) - (len(PLAYER1) % 2)))
-	title2 = (' ' * (BOARD[1] - (len(PLAYER2) / 2))) + '\033[1;37m' + PLAYER2 + '\033[0;37m' + (' ' * (BOARD[1] - (len(PLAYER2) / 2) - (len(PLAYER2) % 2)))
+	title1 = (' ' * (BOARD[1] - (len(PLAYERS[0]) / 2))) + '\033[1;37m' + PLAYERS[0] + '\033[0;37m' + (' ' * (BOARD[1] - (len(PLAYERS[0]) / 2) - (len(PLAYERS[0]) % 2)))
+	title2 = (' ' * (BOARD[1] - (len(PLAYERS[1]) / 2))) + '\033[1;37m' + PLAYERS[1] + '\033[0;37m' + (' ' * (BOARD[1] - (len(PLAYERS[1]) / 2) - (len(PLAYERS[1]) % 2)))
 	print '''
 ┌──────{3}──────┐
 │   {0}  ││  {1}   │
@@ -55,12 +40,12 @@ def draw_board():
 			print '│\033[1;37m{0} \033[0;37m'.format(x+1),
 
 		for y in xrange(BOARD[1]):
-			print '{0}'.format(board_team1[y][x]),
+			print '{0}'.format(board_team[0][y][x]),
 
 		print ' ├┤ ',
 
 		for y in xrange(BOARD[1]):
-			print '{0}'.format(board_team2[y][x]),
+			print '{0}'.format(board_team[1][y][x]),
 
 		if (x+1) < 10:
 			print '\033[1;37m {0} \033[0;37m│'.format(x+1)
@@ -68,6 +53,14 @@ def draw_board():
 			print '\033[1;37m {0}\033[0;37m│'.format(x+1)
 
 	print '└──────{0}──────┘'.format('────' * BOARD[1])
+
+def is_valid(s):
+	try:
+		if (0 < int(s[1:]) <= BOARD[0]) and s[:1] in cols:
+			return True
+		return False
+	except ValueError:
+		return False
 
 def start():
 	fill_board('', '')
@@ -78,7 +71,7 @@ def start():
 	print ''
 
 	if ships > 0 and ships <= limit:
-		print 'TEAM: {0} ({1} naves)'.format(PLAYER1, ships)
+		print 'TEAM: {0} ({1} naves)'.format(PLAYERS[0], ships)
 		for x in xrange(ships):
 			while True:
 				tmp = getpass.getpass("Ingrese coordenada de nave " + str(x+1) + " > ").upper()
@@ -86,15 +79,23 @@ def start():
 					print "Ya se encuentra en tu selección"
 					continue
 
+				if not is_valid(tmp):
+					print "Debe ingresar una coordinada válida"
+					continue
+
 				team[0].append(tmp)
 				break
 
-		print 'TEAM: {0} ({1} naves)'.format(PLAYER2, ships)
+		print 'TEAM: {0} ({1} naves)'.format(PLAYERS[1], ships)
 		for x in xrange(ships):
 			while True:
 				tmp = getpass.getpass("Ingrese coordenada de nave " + str(x+1) + " > ").upper()
 				if tmp in team[1]:
 					print "Ya se encuentra en tu selección"
+					continue
+
+				if not is_valid(tmp):
+					print "Debe ingresar una coordinada válida"
 					continue
 
 				team[1].append(tmp)
@@ -105,22 +106,24 @@ def start():
 
 def play():
 	fill_board('', '')
-
-	turn = ''
+	turn = 1
 
 	while True:
 		if len(team[0]) == 0 or len(team[1]) == 0:
-			print "\033[1;32m¡¡¡ E L  E Q U I P O  '{0}'  H A  G A N A D O !!!\033[0;37m".format(turn)
+			print "\033[1;32m¡¡¡ E L  E Q U I P O  '{0}'  H A  G A N A D O !!!\033[0;37m".format(PLAYERS[turn])
 			break
 
-		if turn == PLAYER1:
-			turn = PLAYER2
-		else:
-			turn = PLAYER1
+		turn = 1 - turn
 
-		shoot = raw_input("[" + turn + "] Ingrese coordenada > ").upper()
+		while True:
+			shoot = raw_input("[" + PLAYERS[turn] + "] Ingrese coordenada > ").upper()
 
-		fill_board(shoot, turn)
+			if shoot not in team_repeat[turn]:
+				fill_board(shoot, turn)
+				break
+			else:
+				print '[ Ya dijiste esa coordenada ]'
+				continue
 
 if __name__ == "__main__":
 	start()
