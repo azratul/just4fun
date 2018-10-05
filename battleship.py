7ed7e9c1489e98daa2bc1ffb03985eb9ef410f4a
Claudio Lepin <azratul@gmail.com>
2018-10-05 09:07:52 -0300

    Standarization

--- battleship.py
+++ battleship.py
@@ -30,32 +30,32 @@ def fill_board(shoot, turn):
 def draw_board():
 	title1 = (' ' * (BOARD[1] - (len(PLAYERS[0]) / 2))) + '\033[1;37m' + PLAYERS[0] + '\033[0;37m' + (' ' * (BOARD[1] - (len(PLAYERS[0]) / 2) - (len(PLAYERS[0]) % 2)))
 	title2 = (' ' * (BOARD[1] - (len(PLAYERS[1]) / 2))) + '\033[1;37m' + PLAYERS[1] + '\033[0;37m' + (' ' * (BOARD[1] - (len(PLAYERS[1]) / 2) - (len(PLAYERS[1]) % 2)))
-	print '''
+	print('''
 ┌─────{3}┬┬{3}─────┐
 │   {0}  ││  {1}   │
 ├─────{3}┼┼{3}─────┤
-│    \033[1;33m{2}\033[0;37m  ├┤  \033[1;33m{2}\033[0;37m    │'''.format(title1, title2, ' '.join(COLS[:BOARD[1]]), ('──' * BOARD[1]))
+│    \033[1;33m{2}\033[0;37m  ├┤  \033[1;33m{2}\033[0;37m    │'''.format(title1, title2, ' '.join(COLS[:BOARD[1]]), ('──' * BOARD[1])))
 
 	for x in xrange(BOARD[0]):
 		if (x+1) < 10:
-			print '│\033[1;33m {0} \033[0;37m'.format(x+1),
+			print('│\033[1;33m {0} \033[0;37m'.format(x+1)),
 		else:
-			print '│\033[1;33m{0} \033[0;37m'.format(x+1),
+			print('│\033[1;33m{0} \033[0;37m'.format(x+1)),
 
 		for y in xrange(BOARD[1]):
-			print '{0}'.format(board_team[0][y][x]),
+			print('{0}'.format(board_team[0][y][x])),
 
-		print ' ├┤ ',
+		print(' ├┤ '),
 
 		for y in xrange(BOARD[1]):
-			print '{0}'.format(board_team[1][y][x]),
+			print('{0}'.format(board_team[1][y][x])),
 
 		if (x+1) < 10:
-			print '\033[1;33m {0} \033[0;37m│'.format(x+1)
+			print('\033[1;33m {0} \033[0;37m│'.format(x+1))
 		else:
-			print '\033[1;33m {0}\033[0;37m│'.format(x+1)
+			print('\033[1;33m {0}\033[0;37m│'.format(x+1))
 
-	print '└─────{0}┴┴{0}─────┘'.format('──' * BOARD[1])
+	print('└─────{0}┴┴{0}─────┘'.format('──' * BOARD[1]))
 
 def is_valid(s):
 	try:
@@ -80,23 +80,23 @@ def start():
 			if 0 < ships <= limit:
 				break
 			else:
-				print 'El número de naves debe ser mayor a 0 y menor o igual a {0}'.format(limit)
+				print('El número de naves debe ser mayor a 0 y menor o igual a {0}'.format(limit))
 				continue
 		except ValueError:
-			print 'Debes ingresar un número'
+			print('Debes ingresar un número')
 			continue
 
 	for key, value in enumerate(PLAYERS):
-		print 'TEAM: {0} ({1} naves)'.format(value, ships)
+		print('TEAM: {0} ({1} naves)'.format(value, ships))
 		for x in xrange(ships):
 			while True:
 				tmp = getpass.getpass("Ingrese coordenada de nave " + str(x+1) + " " + ARROW + " ").upper()
 				if tmp in team[key]:
-					print "Ya se encuentra en tu selección"
+					print("Ya se encuentra en tu selección")
 					continue
 
 				if not is_valid(tmp):
-					print "Debe ingresar una coordinada válida"
+					print("Debe ingresar una coordinada válida")
 					continue
 
 				team[key].append(tmp)
@@ -109,7 +109,7 @@ def play():
 
 	while True:
 		if len(team[0]) == 0 or len(team[1]) == 0:
-			print "\033[1;32m¡¡¡ E L  E Q U I P O  '{0}'  H A  G A N A D O !!!\033[0;37m".format(PLAYERS[turn])
+			print("\033[1;32m¡¡¡ E L  E Q U I P O  '{0}'  H A  G A N A D O !!!\033[0;37m".format(PLAYERS[turn]))
 			time.sleep(4)
 			break
 
@@ -122,7 +122,7 @@ def play():
 				fill_board(shoot, turn)
 				break
 			else:
-				print '[ Ya dijiste esa coordenada ]'
+				print('[ Ya dijiste esa coordenada ]')
 				continue
 
 def settings():
@@ -147,7 +147,7 @@ def settings():
 def menu():
 	while True:
 		os.system('clear')
-		print '''\033[1;31m
+		print('''\033[1;31m
  ╔═══════════════════════════════╗
  ║ ★ ★  B A T T L E S H I P  ★ ★ ║
  ╚═══════════════════════════════╝
@@ -155,7 +155,7 @@ def menu():
  \033[1;37m❶\033[0;37m  J U G A R\033[0;37m
  \033[1;37m❷\033[0;37m  C O N F I G U R A R\033[0;37m
  \033[1;37m❸\033[0;37m  S A L I R\033[0;37m
-'''
+''')
 
 		opt = raw_input(" Ingrese Opción " + ARROW + " ")
 
@@ -164,7 +164,7 @@ def menu():
 		elif opt == "2":
 			settings()
 		elif opt == "3":
-			print '\033[1;32m¡ H A S T A  L U E G O !\033[0;37m'
+			print('\033[1;32m¡ H A S T A  L U E G O !\033[0;37m')
 			break
 
 if __name__ == "__main__":
