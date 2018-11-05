b428b8023355495f4110b1c8b96c01ac7b273143
Claudio Lepin <azratul@gmail.com>
2018-11-05 10:35:52 -0300

    Add some color

--- battleship.py
+++ battleship.py
@@ -15,6 +15,13 @@ team        = [[],[]]
 team_repeat = [[],[]]
 board_team  = [[[GRID for i in range(BOARD[0])] for j in range(BOARD[1])],[[GRID for i in range(BOARD[0])] for j in range(BOARD[1])]]
 
+class Color:
+	white   = '\033[0;37m'
+	redb    = '\033[1;31m'
+	greenb  = '\033[1;32m'
+	yellowb = '\033[1;33m'
+	whiteb  = '\033[1;37m'
+
 def fill_board(shoot, turn):
 	os.system('clear')
 	for x in xrange(BOARD[0]):
@@ -22,26 +29,26 @@ def fill_board(shoot, turn):
 			if shoot == COLS[y] + str(x + 1):
 				if shoot in team[1-turn]:
 					team[1-turn].remove(shoot)
-					board_team[1-turn][y][x] = '\033[1;32m★\033[0;37m'
+					board_team[1-turn][y][x] = Color.greenb + '★' + Color.white
 				else:
-					board_team[1-turn][y][x] = '\033[1;31m✕\033[0;37m'
+					board_team[1-turn][y][x] = Color.redb + '✕' + Color.white
 				team_repeat[turn].append(shoot)
 	draw_board()
 
 def draw_board():
-	title1 = (' ' * (BOARD[1] - (len(PLAYERS[0]) / 2))) + '\033[1;37m' + PLAYERS[0] + '\033[0;37m' + (' ' * (BOARD[1] - (len(PLAYERS[0]) / 2) - (len(PLAYERS[0]) % 2)))
-	title2 = (' ' * (BOARD[1] - (len(PLAYERS[1]) / 2))) + '\033[1;37m' + PLAYERS[1] + '\033[0;37m' + (' ' * (BOARD[1] - (len(PLAYERS[1]) / 2) - (len(PLAYERS[1]) % 2)))
+	title1 = (' ' * (BOARD[1] - (len(PLAYERS[0]) / 2))) + Color.whiteb + PLAYERS[0] + Color.white + (' ' * (BOARD[1] - (len(PLAYERS[0]) / 2) - (len(PLAYERS[0]) % 2)))
+	title2 = (' ' * (BOARD[1] - (len(PLAYERS[1]) / 2))) + Color.whiteb + PLAYERS[1] + Color.white + (' ' * (BOARD[1] - (len(PLAYERS[1]) / 2) - (len(PLAYERS[1]) % 2)))
 	print('''
 ┌─────{3}┬┬{3}─────┐
 │   {0}  ││  {1}   │
 ├─────{3}┼┼{3}─────┤
-│    \033[1;33m{2}\033[0;37m  ├┤  \033[1;33m{2}\033[0;37m    │'''.format(title1, title2, ' '.join(COLS[:BOARD[1]]), ('──' * BOARD[1])))
+│    {5}{2}{4}  ├┤  {5}{2}{4}    │'''.format(title1, title2, ' '.join(COLS[:BOARD[1]]), ('──' * BOARD[1]), Color.white, Color.yellowb))
 
 	for x in xrange(BOARD[0]):
 		if (x+1) < 10:
-			print('│\033[1;33m {0} \033[0;37m'.format(x+1)),
+			print('│{2} {0} {1}'.format(x+1, Color.white, Color.yellowb)),
 		else:
-			print('│\033[1;33m{0} \033[0;37m'.format(x+1)),
+			print('│{2}{0} {1}'.format(x+1, Color.white, Color.yellowb)),
 
 		for y in xrange(BOARD[1]):
 			print('{0}'.format(board_team[0][y][x])),
@@ -52,9 +59,9 @@ def draw_board():
 			print('{0}'.format(board_team[1][y][x])),
 
 		if (x+1) < 10:
-			print('\033[1;33m {0} \033[0;37m│'.format(x+1))
+			print('{2} {0} {1}│'.format(x+1, Color.white, Color.yellowb))
 		else:
-			print('\033[1;33m {0}\033[0;37m│'.format(x+1))
+			print('{2} {0}{1}│'.format(x+1, Color.white, Color.yellowb))
 
 	print('└─────{0}┴┴{0}─────┘'.format('──' * BOARD[1]))
 
@@ -110,14 +117,14 @@ def play():
 
 	while True:
 		if len(team[0]) == 0 or len(team[1]) == 0:
-			print("\033[1;32m¡¡¡ E L  E Q U I P O  '{0}'  H A  G A N A D O !!!\033[0;37m".format(PLAYERS[turn]))
+			print("{2}¡¡¡ E L  E Q U I P O  '{0}'  H A  G A N A D O !!!{1}".format(PLAYERS[turn], Color.white, Color.greenb))
 			time.sleep(4)
 			break
 
 		turn = 1 - turn
 
 		while True:
-			shoot = raw_input("\033[1;37m❮" + PLAYERS[turn] + "❯\033[0;37m Ingrese coordenada " + ARROW + " ").upper()
+			shoot = raw_input(Color.whiteb + "❮" + PLAYERS[turn] + "❯" + Color.white + " Ingrese coordenada " + ARROW + " ").upper()
 
 			if shoot not in team_repeat[turn]:
 				fill_board(shoot, turn)
@@ -148,15 +155,15 @@ def settings():
 def menu():
 	while True:
 		os.system('clear')
-		print('''\033[1;31m
+		print('''{0}
  ╔═══════════════════════════════╗
  ║ ★ ★  B A T T L E S H I P  ★ ★ ║
  ╚═══════════════════════════════╝
- \033[0;37m
- \033[1;37m❶\033[0;37m  J U G A R\033[0;37m
- \033[1;37m❷\033[0;37m  C O N F I G U R A R\033[0;37m
- \033[1;37m❸\033[0;37m  S A L I R\033[0;37m
-''')
+
+ {1}❶{2}  J U G A R
+ {1}❷{2}  C O N F I G U R A R
+ {1}❸{2}  S A L I R
+'''.format(Color.redb, Color.whiteb, Color.white))
 
 		opt = raw_input(" Ingrese Opción " + ARROW + " ")
 
@@ -165,7 +172,7 @@ def menu():
 		elif opt == "2":
 			settings()
 		elif opt == "3":
-			print('\033[1;32m¡ H A S T A  L U E G O !\033[0;37m')
+			print('{0}¡ H A S T A  L U E G O !{1}'.format(Color.greenb, Color.white))
 			break
 
 if __name__ == "__main__":
